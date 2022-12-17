0e06bd1af3b1623cf1fecb79235a08378c1b34d7
YogitaDevi <yogita.gry@gmail.com>
2022-12-17 14:16:55 +0530

    [yogita]An Anagram Detection

---
+++ UC6_AnagramDetection/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using UC6_AnagramDetection;
+
+AnagramDetection anagram = new AnagramDetection();
+anagram.Anagram();
