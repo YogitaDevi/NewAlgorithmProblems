bce7b55b21aa158f3bb80b707141a20e381424df
YogitaDevi <yogita.gry@gmail.com>
2022-12-17 14:14:04 +0530

    [yogita]Merge Sort of list of Strings

---
+++ UC5_MergeSort/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using UC5_MergeSort;
+
+MergeSorting merge = new MergeSorting();
+int[] arrPass = { 55, 89, 214, 62, 150, 66, 99, 33, 11, 45 };
+int len = Convert.ToInt32(arrPass.Length);
+merge.MergeSort(arrPass, 0, len - 1);
+for (int i = 0; i < arrPass.Length; i++)
+{
+    Console.Write(arrPass[i] + " ");
+}
+
